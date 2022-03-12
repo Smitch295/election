d0f73c23be281c5ada0ce8f304e1d77db763bdac
Jazz Shie <jazzshie@Jazzs-iMac.local>
2022-03-12 10:35:29 -0500

    adding files from module 3.

---
+++ app.py
@@ -0,0 +1,2 @@
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+from flask import Flask
---
+++ python_practice1.py
@@ -0,0 +1,6 @@
+temperature = int(input("what is the temperature outside?"))
+if temperature > 80:
+    print ("turn on the ac.")
+else:
+    print ("open the windows.")
+    
